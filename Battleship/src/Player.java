b9840b960d9f7bf879576da8ee98abc07b21dc50
Kritika15 <35933265+KritikaChaudhary@users.noreply.github.com>
2019-07-29 19:55:19 -0400

    Add files via upload

--- Battleship/src/Player.java
+++ Battleship/src/Player.java
@@ -1,225 +1,209 @@
+/*
+ * Class containing player data (fleet location, self and attack data)
+ * @author Group 3
+ * @version 1.2
+ */
+
 import java.util.ArrayList;
-import java.awt.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Player {
+
+	protected Game game;
+	protected String name;
+	protected Screen screen;
+	protected HashMap<String,Ship> fleet;
+	protected int[][] selfData = new int[10][10];	//0=available/not targeted, 1=miss, 2=ship exists&not targeted, 3=hit
+	protected int[][] attackData = new int[10][10];	//0=not yet targeted, 1=targeted shot, 2=hit, 3=miss
 	
-	private int initShips;
-	private Screen screen;
-	private ArrayList<Ship> fleet;
-	private int[][] attackData = new int[11][11];
-    private int[][] selfData = new int[11][11];
-    private int numberOfShipSunk = 0;
-    
-	public Player(String name, Game game, int initShips) {
-		this.initShips = initShips;
-		this.screen = new Screen(name, game);
-		this.fleet = new ArrayList<Ship>();
+	//constructs a new player of name n
+	public Player(String n, Game game) {
+		this.name = n;
+		this.game = game;
+	    fleet = new HashMap<String,Ship>();
+	    for(int i=0;i<10;i++)
+	    	for(int j=0;j<10;j++) {
+	    		selfData[i][j] =0;
+	    		attackData[i][j] =0;
+	    	}
+		System.out.println(name+" created");
 	}
 	
-	public ArrayList<Ship> getFleet() {
-		return this.fleet;
+	//add new screen to this player
+	public void addScreen() {
+		this.screen = new Screen(this);
 	}
-
-	public Screen getScreen() {return this.screen;}
-	public void addShip(Coordinate c1, Coordinate c2, Coordinate c3) {
-		if(c1.getX()==11||c2.getX()==11||c3.getX()==11 ||c1.getY()==11||c2.getY()==11||c3.getY()==11){
-            System.out.print("\nPreventing adding a ship: fleet is full or user clicked too close to the edge");
-        }
-        else {
-            if (isOvelap(c1, c2, c3) ) {
-                if(isOvelapAtRotatePoint(c1,c2,c3) && !isShipCollision(c1,c2,c3)){
-                    //add vertical ship at c1,c2,c3
-                	Coordinate aNew = new Coordinate(c1.getX(), c1.getY());
-                    Coordinate bNew = new Coordinate(c1.getX(), c1.getY() + 1);
-                    Coordinate cNew = new Coordinate(c1.getX(), c1.getY() + 2);
-                    fleet.add(new Ship(aNew, bNew, cNew));
-                    selfData[aNew.getX()][aNew.getY()]=1;
-                    selfData[bNew.getX()][bNew.getY()]=1;
-                    selfData[cNew.getX()][cNew.getY()]=1;
-                }
-                else {
-                    System.out.print("\nyou should only click first block to rotate");
-                }
-
-            } else if(fleet.size() < 5){
-                fleet.add(new Ship(c1, c2, c3));
-                selfData[c1.getX()][c1.getY()]=1;
-                selfData[c2.getX()][c2.getY()]=1;
-                selfData[c3.getX()][c3.getY()]=1;
-            }
-        }
+	//return associated game object
+	public Game getGame() {
+		return this.game;
 	}
-	public boolean isOvelap(Coordinate a, Coordinate b, Coordinate c){
-        boolean isA = false;
-        boolean isB = false;
-        boolean isC = false;
-
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++)
-            {
-                if((a.getX()==i&&a.getY()==j)){
-                    if(selfData[i][j] == 1){
-                        isA = true;
-                    }
-                    else{
-                        isA = false;
-                    }
-                }
-                if(b.getX()==i&&b.getY()==j){
-                    if(selfData[i][j] == 1){
-                        isB = true;
-                    }
-                    else{
-                        isB = false;
-                    }
-                }
-                if(c.getX()==i&&c.getY()==j){
-                    if(selfData[i][j] == 1){
-                        isC = true;
-                    }
-                    else{
-                        isC = false;
-                    }
-                }
-            }
-        }
-        if(isA || isB || isC){
-            return true;
-        }
-        return false;
-    }
-	public boolean isOvelapAtRotatePoint(Coordinate a, Coordinate b, Coordinate c){
-        boolean isA = false;
-        boolean isB = false;
-        boolean isC = false;
-
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++)
-            {
-                if((a.getX()==i&&a.getY()==j)){
-                    if(selfData[i][j] == 1){
-                        isA = true;
-                    }
-                    else{
-                        isA = false;
-                    }
-                }
-                if(b.getX()==i&&b.getY()==j){
-                    if(selfData[i][j] == 1){
-                        isB = true;
-                    }
-                    else{
-                        isB = false;
-                    }
-                }
-                if(c.getX()==i&&c.getY()==j){
-                    if(selfData[i][j] == 1){
-                        isC = true;
-                    }
-                    else{
-                        isC = false;
-                    }
-                }
-            }
-        }
-        if(isA && isB && isC){
-            return true;
-        }
-        return false;
-    }
-	// check for ships collision when ship is rotated
-    public boolean isShipCollision (Coordinate a, Coordinate b, Coordinate c){
-        deleteShip(a,b,c);
-        selfData[a.getX()][a.getY()]=0;
-        selfData[b.getX()][b.getY()]=0;
-        selfData[c.getX()][c.getY()]=0;
-
-        Coordinate aNew = new Coordinate(a.getX(), a.getY());
-        Coordinate bNew = new Coordinate(a.getX(), a.getY() + 1);
-        Coordinate cNew = new Coordinate(a.getX(), a.getY() + 2);
-
-        if(aNew.getX()==11||bNew.getX()==11||cNew.getX()==11 ||aNew.getY()==11||bNew.getY()==11||cNew.getY()==11){
-            addShip(a,b,c);
-            selfData[a.getX()][a.getY()]=1;
-            selfData[b.getX()][b.getY()]=1;
-            selfData[c.getX()][c.getY()]=1;
-            return true;
-        }
-
-        for (int i = 0; i < fleet.size(); i++) {
-            if(fleet.get(i).getA().compareCoord(bNew) || fleet.get(i).getB().compareCoord(bNew) || fleet.get(i).getC().compareCoord(bNew) ||
-                    fleet.get(i).getA().compareCoord(cNew) || fleet.get(i).getB().compareCoord(cNew) || fleet.get(i).getC().compareCoord(cNew)){
-                addShip(a,b,c);
-                selfData[a.getX()][a.getY()]=1;
-                selfData[b.getX()][b.getY()]=1;
-                selfData[c.getX()][c.getY()]=1;
-                return true;
-            }
-        }
-        return false;
-
-    }
-  //remove ship from fleet array
-    public void deleteShip(Coordinate a, Coordinate b, Coordinate c) {
-        Ship temp = new Ship(a, b, c);
-        for (int i = 0; i < fleet.size(); i++) {
-            if (fleet.get(i).compareShip(temp)) {
-                fleet.remove(i);
-            }
-        }
-    }
-	public void attackShip(Coordinate hit) {
-		for(Ship ship : fleet) {
-			ship.Hit(hit);
+	
+	//return name of this player
+	public String getName() {
+		return this.name;
+	}
+	
+	//return screen of this player
+	public Screen getScreen() {
+		return this.screen;
+	}
+	
+	//adds ship of type shipName at coordinates
+	public void addShip(Ship ship, Coordinate[] coordinates) {
+		this.fleet.put(ship.getName(),ship);
+		this.updateSelfData(coordinates,2);
+	}
+	
+	//update data of self board with value at coordinates
+	public void updateSelfData(Coordinate[] coordinates, int value) {
+		for(Coordinate c : coordinates) 
+			this.selfData[c.getY()][c.getX()] = value;
+		printSelfData();
+	}
+	
+	//return true if ship of type shipName exists in its fleet, else return false
+	public boolean containsShip(String shipName) {
+		return this.fleet.containsKey(shipName);
+	}
+	
+	//delete and return ship of type shipName from this player's fleet
+	public Ship deleteShip(String shipName) {
+		updateSelfData(fleet.get(shipName).getLocation(),0);
+		return this.fleet.remove(shipName);
+		
+	}
+	
+	//check if this ship can be placed at (x,y) with alignment align on board
+	public boolean checkPossible(String shipName, int shipSize, int x, int y, Alignment align) {
+		int endX, endY;
+		if(align.equals(Alignment.HORIZONTAL)) {
+			endX = x + shipSize - 1;
+			endY = y;
+		}
+		else {
+			endX = x;
+			endY = y + shipSize - 1;
+		}
+		System.out.print("checking from "+x+","+y+" to "+endX+","+endY+"--");
+		if(endX >= 10 || endY >= 10) {	//edge constraints
+			System.out.println("edge");
+			return false;	
+		}
+		//check for collision : if this ship exists in fleet, disregard first coordinate
+		for(int i = x;i<=endX;i++)
+			for(int j = y;j<=endY;j++) {
+				System.out.print(i+","+j+":");
+				if(!(this.fleet.containsKey(shipName) && i==x && j==y) && selfData[j][i]==2) {
+					System.out.println("collision");
+					return false;
+				}
+				System.out.println("no collision");
+			}
+		return true;
+	}
+	
+	
+	
+	//get first coordinate of this ship
+	public Coordinate getFirstCoordinate(String shipName) {
+		Ship ship = fleet.get(shipName);
+		return ship.getLocation()[0];
+	}
+	
+	//get data of self board where 0=available/not targeted, 1=miss, 2=ship exists&not targeted, 3=hit
+	public int[][] getSelfData(){
+		return this.selfData;
+	}
+	
+	//return ship of type shipName from fleet
+	public Ship getShip(String shipName) {
+		return fleet.get(shipName);
+	}
+	
+	//print self data for debugging
+	public void printSelfData() {
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++)
+				System.out.print(selfData[i][j]+" ");
+			System.out.println();
 		}
 	}
-	public void setAttackData(int x, int y, String result) {
-        if(result.equals("success")){
-            attackData[x][y] = 1;
-        }
-        else if(result.equals("failure")){
-            attackData[x][y] = 2;
-        }
-    }
-	public int[][] getAttackData(){return this.attackData;}
-	public int[][] getSelfData(){return this.selfData;}
-	public int getNumberOfOwnShipSunk() {return this.numberOfShipSunk;}
-	public boolean isHit(Coordinate point){
-        for (int i=0;i<fleet.size();){
-            Ship temp = fleet.get(i);
-            if(temp.isPointHit(point)){
-                return true;
-            }
-            else{
-                i++; // increase the counter if the the ship i didn't contain a point
-            }
-        }
-        return false; // must return false because
-    }
+	
+	//get fleet size of this player
+	public int getFleetSize() {
+		return this.fleet.keySet().size();
+	}
+	
+	/*
+	 * return true if Coordinate c is hit, else return false. 
+	 * Update selfData and opponent's attackData where applicable
+	 * Update ship's location hashmap 
+	 */
+	public boolean hit(Coordinate c) {
+		System.out.print("Hitting "+name+" at Coordinate:"+c.getX()+","+c.getY()+"--");
+		for(Ship ship : fleet.values()) {
+			if(ship.hit(c)) {
+				selfData[c.getY()][c.getX()] = 3;
+				this.game.getOppo(this).setAttackData(c.getX(), c.getY(), dataValue.HIT);
+				System.out.println(name+"'s self data:");
+				printSelfData();
+				System.out.println("Opponent's attack data:");
+				this.game.getOppo(this).printAttackData();
+				return true;
+			}
+		}
+		selfData[c.getY()][c.getX()] = 1;
+		game.getOppo(this).setAttackData(c.getX(), c.getY(), dataValue.MISS);
+		System.out.println(name+"'s self data:");
+		printSelfData();
+		System.out.println("Opponent's attack data:");
+		this.game.getOppo(this).printAttackData();
+		return false;
+	}
+	
+	//get array of ships that are sunk
+	public LinkedList<Ship> getSunkShips() {
+		LinkedList<Ship> sunkShips = new LinkedList<Ship>();
+		for(Ship ship : fleet.values())
+			if(ship.isSunk())
+				sunkShips.add(ship);
+		return sunkShips;
+	}
 
-    // isPlayerLost determines if the player lost based on the fleet arraylist size
-    public boolean isPlayerLost(){
-        if(fleet.size()==0){
-            return true; // Player lost
-        }
-        else{
-            return false;
-        }
-    }
+	//set player's attackData[x][y] to corresponding value for dataValue(0=not yet targeted, 1=targeted shot, 2=hit, 3=miss)
+	public void setAttackData(int x, int y, dataValue d) {
+		int temp =0;
+		switch(d) {
+		case NOT_TARGETED : 
+			temp =0; 
+			break;
+		case SHOT :
+			temp =1;
+			break;
+		case HIT :
+			temp =2;
+			break;
+		case MISS :
+			temp=3;
+			break;
+		}
+		attackData[y][x] = temp;
+		printAttackData();
+	}
 
-    // Boolean method returns true if the Ship was sunk
-    public boolean isSunk(Coordinate hitCord){
-        for (int i=0;i<fleet.size();){
-            Ship temp = fleet.get(i);
-            if(temp.isShipSunk()){
-                numberOfShipSunk++;
-                fleet.remove(i);
-                return true;
-            }
-            else{
-                i++; // increase the counter if the ship wasn't sunk
-            }
-        }
-        return false;
-    }
+	//get player's attackData
+	public int[][] getAttackData() {
+		return this.attackData;
+	}
+
+	//print this player's attackData for debugging purposes
+	public void printAttackData() {
+		for(int i=0;i<10;i++) {
+			for(int j=0;j<10;j++)
+				System.out.print(attackData[i][j]+" ");
+			System.out.println();
+		}
+	}
+	
 }
